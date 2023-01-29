a5aae0b2a223bebb5a059612f822bbb6069f7d3e
Alex123198 <alexandru.negrea98@e-uvt.ro>
2023-01-29 18:46:36 +0200

    Move thesis list to the dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./components/login/login.component";
 import {SignupComponent} from "./components/signup/signup.component";
 import {HomeComponent} from "./components/home-page/home/home.component";
+import {DashboardComponent} from "./components/home-page/dashboard/dashboard.component";
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -6,11 +8,13 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  type: string = "password"
-  isText: boolean = false
-  eyeIcon: string = "fa fa-eye-slash"
+  type: string = "password";
+  isText: boolean = false;
+  eyeIcon: string = "fa fa-eye-slash";
+  username: string | undefined;
+  password: string | undefined;
 
-  constructor() {
+  constructor(private router: Router) {
   }
 
   ngOnInit(): void {
@@ -20,5 +24,12 @@ export class LoginComponent implements OnInit {
     this.isText = !this.isText;
     this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
     this.isText ? this.type = "text" : this.type = "password";
+  }
+
+  login(formData: NgForm) {
+    this.username = formData.value.username;
+    this.password = formData.value.password;
+    console.log(this.password, this.username);
+    this.router.navigateByUrl("/home");
   }
 }
